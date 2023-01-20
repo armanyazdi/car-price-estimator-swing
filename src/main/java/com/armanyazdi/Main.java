4358e002d66179d9cbc3123ddb636ccef5cf3b20
Arman Yazdi <f41c0n.32@gmail.com>
2023-01-21 02:56:28 +0330

    Update Main.java

--- src/main/java/com/armanyazdi/Main.java
+++ src/main/java/com/armanyazdi/Main.java
@@ -2,8 +2,8 @@ package com.armanyazdi;
 
 public class Main {
     public static void main(String[] args) {
-        Estimator estimator = new Estimator();
-        estimator.userInput();
-        estimator.priceEstimator();
+        Estimator carPriceEstimator = new Estimator();
+        carPriceEstimator.userInput();
+        carPriceEstimator.priceEstimator();
     }
 }
