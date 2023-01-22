fd773d846f0a38226300433ebba989f77b9babfb
Arman Yazdi <f41c0n.32@gmail.com>
2023-01-23 03:21:05 +0330

    Update Main.java

--- src/main/java/com/armanyazdi/Main.java
+++ src/main/java/com/armanyazdi/Main.java
@@ -1,9 +1,11 @@
 package com.armanyazdi;
 
+import java.awt.*;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FontFormatException {
         Estimator carPriceEstimator = new Estimator();
-        carPriceEstimator.userInput();
-        carPriceEstimator.priceEstimator();
+        carPriceEstimator.mainFrame();
     }
 }
